3df71fdcc15f1668ef9ef73de5284d85591213ca
Lawrence <lawrence.goldstien@madetech.com>
2019-12-29 14:33:37 +0000

    Add bootstrap to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.scss';
+import {Container} from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Container/>
   );
 }
 
